bbbe0c273da598a9404147ec2828e6f407ee9b94
Malte311 <malte.luttermann@student.uni-luebeck.de>
2021-03-19 16:41:40 +0100

    Update logging.jl

--- logging.jl
+++ logging.jl
@@ -1,22 +1,31 @@
 using Dates
 using Logging
 
-function write_log(msg::String, lvl::Logging.LogLevel)
+"""
+	writelog(msg::String, lvl::Logging.LogLevel)
+
+Write a log message to stdout and additionally to a file if the
+provided parameter lvl is of type Logging.Error.
+
+# Examples
+```julia
+julia> writelog("Hello, world!", Logging.Info)
+```
+"""
+function writelog(msg::String, lvl::Logging.LogLevel)
 	io = open("log.txt", "a+")
 	logger = SimpleLogger(io, Logging.Error)
 
 	msg = "[$(Dates.format(Dates.now(), "dd.mm.Y HH:MM:SS"))] $msg"
 
+	# Log to file
 	with_logger(logger) do
 		@logmsg lvl msg
 	end
 
+	# Log to stdout
 	@logmsg lvl msg
 
 	flush(io)
 	close(io)
 end
-
-write_log("Hallo!", Logging.Info)
-write_log("Hallo!", Logging.Error)
-write_log("Hallo!", Logging.Warn)
