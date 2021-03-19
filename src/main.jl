c7d7a4e1d166d0778f878b48755dd71b6d483c1c
Malte311 <malte.luttermann@student.uni-luebeck.de>
2021-03-19 16:56:37 +0100

    Move some files

---
+++ src/main.jl
@@ -0,0 +1,22 @@
+using LightGraphs
+using GraphPlot
+
+# g = Graph(5)
+
+# add_edge!(g, 1, 2)
+# add_edge!(g, 2, 3)
+# add_edge!(g, 3, 4)
+# add_edge!(g, 4, 5)
+# add_edge!(g, 5, 1)
+
+# gplot(g, nodelabel=1:5)
+
+# for arg in ARGS
+# 	config = open("arg", "r")
+	
+
+
+# 	println(arg)
+# end
+
+print("Hallo!")
