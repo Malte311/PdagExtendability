122f65ad4a0887eaf6ff7e97c83e696da7ab18cd
Malte311 <malte.luttermann@student.uni-luebeck.de>
2021-04-01 10:29:10 +0200

    Enable calling of degeneracy ordering

--- src/extendability/new_algo.jl
+++ src/extendability/new_algo.jl
@@ -1,44 +1,58 @@
 using LightGraphs
 
 include("new_algo_datastructure.jl")
+include("new_algo_optimization.jl")
 
 """
+	fastpdag2dag(g::SimpleDiGraph, optimize::Bool = false)::SimpleDiGraph
+
 Convert a partially directed acyclic graph (PDAG) into a fully
 directed acyclic graph (DAG). If this is not possible, an empty
 graph is returned.
 
 Undirected edges are represented as two directed edges.
 
-# References
-TODO
+If the parameter optimize is omitted or set to false, the algorithm runs in
+time O(Δm) with Δ being the maximum degree of g and m the number of edges in
+g. Setting optimize to true will yield an algorithm in time O(dm), where d
+is the degeneracy of the skeleton.
 
 # Examples
-TODO
+```julia-repl
+julia> g = SimpleDiGraph(3)
+{3, 0} directed simple Int64 graph
+julia> add_edge!(g, 1, 2)
+true
+julia> add_edge!(g, 2, 3)
+true
+julia> add_edge!(g, 3, 2)
+true
+julia> dag = fastpdag2dag(g)
+{3, 2} directed simple Int64 graph
+julia> collect(edges(dag))
+2-element Array{LightGraphs.SimpleGraphs.SimpleEdge{Int64},1}:
+ Edge 1 => 2
+ Edge 2 => 3
+julia> dag = fastpdag2dag(g, true)
+{3, 2} directed simple Int64 graph
+julia> collect(edges(dag))
+2-element Array{LightGraphs.SimpleGraphs.SimpleEdge{Int64},1}:
+ Edge 1 => 2
+ Edge 2 => 3
+```
 """
-function fastpdag2dag(g::SimpleDiGraph)::SimpleDiGraph
+function fastpdag2dag(g::SimpleDiGraph, optimize::Bool = false)::SimpleDiGraph
 	result = copy(g)
-	hg = init(nv(g))
-
+	
 	# Set up the datastructure.
-	undirected = Set{String}()
-	for edge in edges(g)
-		isundirected = has_edge(g, edge.dst, edge.src)
-
-		if isundirected
-			key = "$(edge.src)-$(edge.dst)"
-			!(key in undirected) && insert_edge!(hg, edge.src, edge.dst)
-			push!(undirected, join(reverse(split(key, "-")), "-"))
-		else
-			insert_arc!(hg, edge.src, edge.dst)
-		end
-	end
+	hg = optimize ? optimizedsetup(g) : standardsetup(g)
 
 	ps = list_ps(hg)
 
 	while !isempty(ps)
 		s = pop!(ps)
 
-		# Direct all edges incident to ps towards ps
+		# Direct all edges incident to ps towards ps.
 		isassigned(hg.g1.adjlist, s) || (hg.g1.adjlist[s] = Set{Int64}())
 		for undirected in hg.g1.adjlist[s]
 			rem_edge!(result, s, undirected)
@@ -55,4 +69,118 @@ function fastpdag2dag(g::SimpleDiGraph)::SimpleDiGraph
 	sum(hg.g1.deltaplus) + sum(hg.g2.deltaplus) == 0 || return SimpleDiGraph(0)
 
 	result
+end
+
+"""
+	standardsetup(g::SimpleDiGraph)::HybridGraph
+
+Set up the datastructure for the algorithm with time complexity O(Δm).
+
+# Examples
+```julia-repl
+julia> g = SimpleDiGraph(3)
+{3, 0} directed simple Int64 graph
+julia> add_edge!(g, 1, 2)
+true
+julia> add_edge!(g, 2, 3)
+true
+julia> add_edge!(g, 3, 2)
+true
+julia> standardsetup(g)
+HybridGraph(
+	DirectedGraph(
+		Set{Int64}[Set(), Set([3]), Set([2])],
+		[0, 1, 1],
+		[0, 1, 1],
+		Set{Int64}[#undef, Set([3]), Set([2])],
+		Set{Int64}[#undef, Set([3]), Set([2])]
+	),
+	DirectedGraph(
+		Set{Int64}[Set([2]), Set([1]), Set()],
+		[1, 0, 0],
+		[0, 1, 0],
+		Set{Int64}[#undef, Set([1]), #undef],
+		Set{Int64}[Set([2]), Set(), Set()]
+	),
+	[0, 0, 0],
+	[0, 0, 0]
+)
+```
+"""
+function standardsetup(g::SimpleDiGraph)::HybridGraph
+	hg = init(nv(g))
+	done = Set{String}()
+
+	for e in edges(g)
+		isundirected = has_edge(g, e.dst, e.src)
+
+		if isundirected
+			key = "$(e.src)-$(e.dst)"
+			!("$(e.src)-$(e.dst)" in done) && insert_edge!(hg, e.src, e.dst)
+			push!(done, "$(e.dst)-$(e.src)") # Mark edge as done
+		else
+			insert_arc!(hg, e.src, e.dst)
+		end
+	end
+
+	hg
+end
+
+"""
+	optimizedsetup(g::SimpleDiGraph)::HybridGraph
+
+Set up the datastructure for the algorithm with time complexity O(dm).
+
+# Examples
+```julia-repl
+julia> g = SimpleDiGraph(3)
+{3, 0} directed simple Int64 graph
+julia> add_edge!(g, 1, 2)
+true
+julia> add_edge!(g, 2, 3)
+true
+julia> add_edge!(g, 3, 2)
+true
+julia> optimizedsetup(g)
+HybridGraph(
+	DirectedGraph(
+		Set{Int64}[Set(), Set([3]), Set([2])],
+		[0, 1, 1],
+		[0, 1, 1],
+		Set{Int64}[#undef, Set([3]), Set([2])],
+		Set{Int64}[#undef, Set([3]), Set([2])]
+	),
+	DirectedGraph(
+		Set{Int64}[Set([2]), Set([1]), Set()],
+		[1, 0, 0],
+		[0, 1, 0],
+		Set{Int64}[#undef, Set([1]), #undef],
+		Set{Int64}[Set([2]), Set(), Set()]
+	),
+	[0, 0, 0],
+	[0, 0, 0]
+)
+```
+"""
+function optimizedsetup(g::SimpleDiGraph)::HybridGraph
+	hg = init(nv(g))
+	done = Set{String}()
+
+	for v in degeneracy_ordering(g)
+		for adj in all_neighbors(g, v)
+			adj < v || continue # Insert edges to preceding neighbors only
+
+			is_ingoing = has_edge(g, adj, v)
+			is_outgoing = has_edge(g, v, adj)
+			if is_ingoing && is_outgoing # Edge is undirected
+				!("$v-$adj" in done) && insert_edge!(hg, v, adj)
+				push!(done, "$adj-$v") # Mark edge as done
+			else # Edge is directed
+				is_ingoing && insert_arc!(hg, adj, v)
+				is_outgoing && insert_arc!(hg, v, adj)
+			end
+		end
+	end
+
+	hg
 end
