d05e4aae4941b3a6f15ded2ab14af5d7195d1859
Malte311 <malte.luttermann@student.uni-luebeck.de>
2021-04-02 10:37:34 +0200

    Renames ('in' is a keyword..)

--- test/extendability/test_dor_tarsi_algo.jl
+++ test/extendability/test_dor_tarsi_algo.jl
@@ -1,88 +1,88 @@
 @testset "pdag2dag" begin
 	@testset "No changes for DAG inputs" begin
-		in = SimpleDiGraph(2)
-		add_edge!(in, 1, 2)
-		out = pdag2dag(in)
+		input = SimpleDiGraph(2)
+		add_edge!(input, 1, 2)
+		out = pdag2dag(input)
 		@test nv(out) == 2 && ne(out) == 1 && has_edge(out, 1, 2)
 
-		in = SimpleDiGraph(3)
-		add_edge!(in, 1, 2)
-		add_edge!(in, 2, 3)
-		out = pdag2dag(in)
+		input = SimpleDiGraph(3)
+		add_edge!(input, 1, 2)
+		add_edge!(input, 2, 3)
+		out = pdag2dag(input)
 		@test nv(out) == 3 && ne(out) == 2 && has_edge(out, 1, 2) &&
 			has_edge(out, 2, 3)
 	end
 
 	@testset "Meek Rule R1" begin
-		in = SimpleDiGraph(3)
-		add_edge!(in, 1, 2)
-		add_edge!(in, 2, 3)
-		add_edge!(in, 3, 2)
-		out = pdag2dag(in)
+		input = SimpleDiGraph(3)
+		add_edge!(input, 1, 2)
+		add_edge!(input, 2, 3)
+		add_edge!(input, 3, 2)
+		out = pdag2dag(input)
 		@test nv(out) == 3 && ne(out) == 2 && has_edge(out, 1, 2) &&
 			has_edge(out, 2, 3)
 	end
 
 	@testset "Meek Rule R2" begin
-		in = SimpleDiGraph(3)
-		add_edge!(in, 1, 2)
-		add_edge!(in, 1, 3)
-		add_edge!(in, 3, 1)
-		add_edge!(in, 2, 3)
-		out = pdag2dag(in)
+		input = SimpleDiGraph(3)
+		add_edge!(input, 1, 2)
+		add_edge!(input, 1, 3)
+		add_edge!(input, 3, 1)
+		add_edge!(input, 2, 3)
+		out = pdag2dag(input)
 		@test nv(out) == 3 && ne(out) == 3 && has_edge(out, 1, 2) &&
 			has_edge(out, 1, 3) && has_edge(out, 2, 3)
 	end
 
 	@testset "Meek Rule R3" begin
-		in = SimpleDiGraph(4)
-		add_edge!(in, 1, 2)
-		add_edge!(in, 2, 1)
-		add_edge!(in, 1, 3)
-		add_edge!(in, 3, 1)
-		add_edge!(in, 1, 4)
-		add_edge!(in, 4, 1)
-		add_edge!(in, 2, 3)
-		add_edge!(in, 4, 3)
-		out = pdag2dag(in)
+		input = SimpleDiGraph(4)
+		add_edge!(input, 1, 2)
+		add_edge!(input, 2, 1)
+		add_edge!(input, 1, 3)
+		add_edge!(input, 3, 1)
+		add_edge!(input, 1, 4)
+		add_edge!(input, 4, 1)
+		add_edge!(input, 2, 3)
+		add_edge!(input, 4, 3)
+		out = pdag2dag(input)
 		@test nv(out) == 4 && ne(out) == 5 && has_edge(out, 1, 2) &&
 			has_edge(out, 1, 3) && has_edge(out, 2, 3) && has_edge(out, 4, 1) &&
 			has_edge(out, 4, 3)
 	end
 
 	@testset "Meek Rule R4" begin
-		in = SimpleDiGraph(4)
-		add_edge!(in, 1, 2)
-		add_edge!(in, 2, 1)
-		add_edge!(in, 1, 3)
-		add_edge!(in, 3, 1)
-		add_edge!(in, 1, 4)
-		add_edge!(in, 4, 1)
-		add_edge!(in, 3, 2)
-		add_edge!(in, 4, 3)
-		out = pdag2dag(in)
+		input = SimpleDiGraph(4)
+		add_edge!(input, 1, 2)
+		add_edge!(input, 2, 1)
+		add_edge!(input, 1, 3)
+		add_edge!(input, 3, 1)
+		add_edge!(input, 1, 4)
+		add_edge!(input, 4, 1)
+		add_edge!(input, 3, 2)
+		add_edge!(input, 4, 3)
+		out = pdag2dag(input)
 		@test nv(out) == 4 && ne(out) == 5 && has_edge(out, 1, 2) &&
 			has_edge(out, 3, 1) && has_edge(out, 3, 2) && has_edge(out, 4, 1) &&
 			has_edge(out, 4, 3)
 	end
 
 	@testset "More PDAGs with possible extensions" begin
-		in = SimpleDiGraph(4)
-		add_edge!(in, 1, 2)
-		add_edge!(in, 2, 3)
-		add_edge!(in, 3, 2)
-		add_edge!(in, 3, 4)
-		out = pdag2dag(in)
+		input = SimpleDiGraph(4)
+		add_edge!(input, 1, 2)
+		add_edge!(input, 2, 3)
+		add_edge!(input, 3, 2)
+		add_edge!(input, 3, 4)
+		out = pdag2dag(input)
 		@test nv(out) == 4 && ne(out) == 3 && has_edge(out, 1, 2) &&
 		has_edge(out, 2, 3) && has_edge(out, 3, 4)
 
 		for n in [20, 50, 100]
-			in = SimpleDiGraph(n)
+			input = SimpleDiGraph(n)
 			for i = 1:n-1
-				add_edge!(in, i, i+1)
-				i % 2 == 0 && add_edge!(in, i+1, i)
+				add_edge!(input, i, i+1)
+				i % 2 == 0 && add_edge!(input, i+1, i)
 			end
-			out = pdag2dag(in)
+			out = pdag2dag(input)
 			@test nv(out) == n && ne(out) == n-1
 			isok = true
 			for i = 1:n-1
@@ -93,39 +93,39 @@
 	end
 
 	@testset "Empty graph if no consistent extension is possible" begin
-		in = SimpleDiGraph(4)
-		add_edge!(in, 1, 2)
-		add_edge!(in, 2, 1)
-		add_edge!(in, 1, 3)
-		add_edge!(in, 3, 1)
-		add_edge!(in, 2, 4)
-		add_edge!(in, 4, 2)
-		add_edge!(in, 3, 4)
-		add_edge!(in, 4, 3)
-		out = pdag2dag(in)
+		input = SimpleDiGraph(4)
+		add_edge!(input, 1, 2)
+		add_edge!(input, 2, 1)
+		add_edge!(input, 1, 3)
+		add_edge!(input, 3, 1)
+		add_edge!(input, 2, 4)
+		add_edge!(input, 4, 2)
+		add_edge!(input, 3, 4)
+		add_edge!(input, 4, 3)
+		out = pdag2dag(input)
 		@test nv(out) == 0 && ne(out) == 0
 
-		in = SimpleDiGraph(4)
-		add_edge!(in, 1, 4)
-		add_edge!(in, 2, 1)
-		add_edge!(in, 2, 4)
-		add_edge!(in, 4, 2)
-		add_edge!(in, 3, 1)
-		add_edge!(in, 3, 4)
-		add_edge!(in, 4, 3)
-		out = pdag2dag(in)
+		input = SimpleDiGraph(4)
+		add_edge!(input, 1, 4)
+		add_edge!(input, 2, 1)
+		add_edge!(input, 2, 4)
+		add_edge!(input, 4, 2)
+		add_edge!(input, 3, 1)
+		add_edge!(input, 3, 4)
+		add_edge!(input, 4, 3)
+		out = pdag2dag(input)
 		@test nv(out) == 0 && ne(out) == 0
 	end
 end
 
 @testset "sink" begin
-	in = SimpleDiGraph(3)
-	add_edge!(in, 1, 2)
-	add_edge!(in, 2, 3)
-	add_edge!(in, 3, 2)
-	@test 3 == sink(in)
-	rem_vertex!(in, 3)
-	@test 2 == sink(in)
-	rem_vertex!(in, 2)
-	@test 1 == sink(in)
+	input = SimpleDiGraph(3)
+	add_edge!(input, 1, 2)
+	add_edge!(input, 2, 3)
+	add_edge!(input, 3, 2)
+	@test 3 == sink(input)
+	rem_vertex!(input, 3)
+	@test 2 == sink(input)
+	rem_vertex!(input, 2)
+	@test 1 == sink(input)
 end
--- test/extendability/test_new_algo.jl
+++ test/extendability/test_new_algo.jl
@@ -1,15 +1,15 @@
 @testset "fastpdag2dag" begin
 	@testset "No changes for DAG inputs" begin
 		for optimize in [true, false]
-			in = SimpleDiGraph(2)
-			add_edge!(in, 1, 2)
-			out = fastpdag2dag(in, optimize)
+			input = SimpleDiGraph(2)
+			add_edge!(input, 1, 2)
+			out = fastpdag2dag(input, optimize)
 			@test nv(out) == 2 && ne(out) == 1 && has_edge(out, 1, 2)
 
-			in = SimpleDiGraph(3)
-			add_edge!(in, 1, 2)
-			add_edge!(in, 2, 3)
-			out = fastpdag2dag(in, optimize)
+			input = SimpleDiGraph(3)
+			add_edge!(input, 1, 2)
+			add_edge!(input, 2, 3)
+			out = fastpdag2dag(input, optimize)
 			@test nv(out) == 3 && ne(out) == 2 && has_edge(out, 1, 2) &&
 				has_edge(out, 2, 3)
 		end
@@ -17,11 +17,11 @@
 
 	@testset "Meek Rule R1" begin
 		for optimize in [true, false]
-			in = SimpleDiGraph(3)
-			add_edge!(in, 1, 2)
-			add_edge!(in, 2, 3)
-			add_edge!(in, 3, 2)
-			out = fastpdag2dag(in, optimize)
+			input = SimpleDiGraph(3)
+			add_edge!(input, 1, 2)
+			add_edge!(input, 2, 3)
+			add_edge!(input, 3, 2)
+			out = fastpdag2dag(input, optimize)
 			@test nv(out) == 3 && ne(out) == 2 && has_edge(out, 1, 2) &&
 				has_edge(out, 2, 3)
 		end
@@ -29,12 +29,12 @@
 
 	@testset "Meek Rule R2" begin
 		for optimize in [true, false]
-			in = SimpleDiGraph(3)
-			add_edge!(in, 1, 2)
-			add_edge!(in, 1, 3)
-			add_edge!(in, 3, 1)
-			add_edge!(in, 2, 3)
-			out = fastpdag2dag(in, optimize)
+			input = SimpleDiGraph(3)
+			add_edge!(input, 1, 2)
+			add_edge!(input, 1, 3)
+			add_edge!(input, 3, 1)
+			add_edge!(input, 2, 3)
+			out = fastpdag2dag(input, optimize)
 			@test nv(out) == 3 && ne(out) == 3 && has_edge(out, 1, 2) &&
 				has_edge(out, 1, 3) && has_edge(out, 2, 3)
 		end
@@ -61,16 +61,16 @@
 
 	@testset "Meek Rule R4" begin
 		for optimize in [true, false]
-			in = SimpleDiGraph(4)
-			add_edge!(in, 1, 2)
-			add_edge!(in, 2, 1)
-			add_edge!(in, 1, 3)
-			add_edge!(in, 3, 1)
-			add_edge!(in, 1, 4)
-			add_edge!(in, 4, 1)
-			add_edge!(in, 3, 2)
-			add_edge!(in, 4, 3)
-			out = fastpdag2dag(in, optimize)
+			input = SimpleDiGraph(4)
+			add_edge!(input, 1, 2)
+			add_edge!(input, 2, 1)
+			add_edge!(input, 1, 3)
+			add_edge!(input, 3, 1)
+			add_edge!(input, 1, 4)
+			add_edge!(input, 4, 1)
+			add_edge!(input, 3, 2)
+			add_edge!(input, 4, 3)
+			out = fastpdag2dag(input, optimize)
 			@test nv(out) == 4 && ne(out) == 5 && has_edge(out, 1, 2) &&
 				has_edge(out, 3, 1) && has_edge(out, 3, 2) && has_edge(out, 4, 1) &&
 				has_edge(out, 4, 3)
@@ -79,22 +79,22 @@
 
 	@testset "More PDAGs with possible extensions" begin
 		for optimize in [true, false]
-			in = SimpleDiGraph(4)
-			add_edge!(in, 1, 2)
-			add_edge!(in, 2, 3)
-			add_edge!(in, 3, 2)
-			add_edge!(in, 3, 4)
-			out = fastpdag2dag(in, optimize)
+			input = SimpleDiGraph(4)
+			add_edge!(input, 1, 2)
+			add_edge!(input, 2, 3)
+			add_edge!(input, 3, 2)
+			add_edge!(input, 3, 4)
+			out = fastpdag2dag(input, optimize)
 			@test nv(out) == 4 && ne(out) == 3 && has_edge(out, 1, 2) &&
 			has_edge(out, 2, 3) && has_edge(out, 3, 4)
 
 			for n in [20, 50, 100]
-				in = SimpleDiGraph(n)
+				input = SimpleDiGraph(n)
 				for i = 1:n-1
-					add_edge!(in, i, i+1)
-					i % 2 == 0 && add_edge!(in, i+1, i)
+					add_edge!(input, i, i+1)
+					i % 2 == 0 && add_edge!(input, i+1, i)
 				end
-				out = fastpdag2dag(in, optimize)
+				out = fastpdag2dag(input, optimize)
 				@test nv(out) == n && ne(out) == n-1
 				isok = true
 				for i = 1:n-1
@@ -107,112 +107,146 @@
 
 	@testset "Empty graph if no consistent extension is possible" begin
 		for optimize in [true, false]
-			in = SimpleDiGraph(4)
-			add_edge!(in, 1, 2)
-			add_edge!(in, 2, 1)
-			add_edge!(in, 1, 3)
-			add_edge!(in, 3, 1)
-			add_edge!(in, 2, 4)
-			add_edge!(in, 4, 2)
-			add_edge!(in, 3, 4)
-			add_edge!(in, 4, 3)
-			out = fastpdag2dag(in, optimize)
+			input = SimpleDiGraph(4)
+			add_edge!(input, 1, 2)
+			add_edge!(input, 2, 1)
+			add_edge!(input, 1, 3)
+			add_edge!(input, 3, 1)
+			add_edge!(input, 2, 4)
+			add_edge!(input, 4, 2)
+			add_edge!(input, 3, 4)
+			add_edge!(input, 4, 3)
+			out = fastpdag2dag(input, optimize)
 			@test nv(out) == 0 && ne(out) == 0
 
-			in = SimpleDiGraph(4)
-			add_edge!(in, 1, 4)
-			add_edge!(in, 2, 1)
-			add_edge!(in, 2, 4)
-			add_edge!(in, 4, 2)
-			add_edge!(in, 3, 1)
-			add_edge!(in, 3, 4)
-			add_edge!(in, 4, 3)
-			out = fastpdag2dag(in, optimize)
+			input = SimpleDiGraph(4)
+			add_edge!(input, 1, 4)
+			add_edge!(input, 2, 1)
+			add_edge!(input, 2, 4)
+			add_edge!(input, 4, 2)
+			add_edge!(input, 3, 1)
+			add_edge!(input, 3, 4)
+			add_edge!(input, 4, 3)
+			out = fastpdag2dag(input, optimize)
 			@test nv(out) == 0 && ne(out) == 0
 		end
 	end
 end
 
 @testset "standardsetup" begin
-	ingraph = SimpleDiGraph(3)
-	add_edge!(ingraph, 1, 2)
-	add_edge!(ingraph, 2, 3)
-	add_edge!(ingraph, 3, 2)
-	g = standardsetup(ingraph)
-	for i = 1:3
-		@test 0 == g.alpha[i]
-		@test 0 == g.beta[i]
+	@testset "All values are set correctly" begin
+		ingraph = SimpleDiGraph(3)
+		add_edge!(ingraph, 1, 2)
+		add_edge!(ingraph, 2, 3)
+		add_edge!(ingraph, 3, 2)
+		g = standardsetup(ingraph)
+		for i = 1:3
+			@test 0 == g.alpha[i]
+			@test 0 == g.beta[i]
+		end
+		@test (1 in g.g2.adjlist[2]) && 1 == length(g.g2.adjlist[2])
+		@test (2 in g.g2.adjlist[1]) && 1 == length(g.g2.adjlist[1])
+		@test !isassigned(g.g2.adjlist, 3) || isempty(g.g2.adjlist[3])
+		@test !isassigned(g.g1.adjlist, 1) || isempty(g.g1.adjlist[1])
+		@test (3 in g.g1.adjlist[2]) && 1 == length(g.g1.adjlist[2])
+		@test (2 in g.g1.adjlist[3]) && 1 == length(g.g1.adjlist[3])
+		@test (1 in g.g2.ingoing[2]) && 1 == length(g.g2.ingoing[2])
+		@test !isassigned(g.g2.ingoing, 1) || isempty(g.g2.ingoing[1])
+		@test !isassigned(g.g2.ingoing, 3) || isempty(g.g2.ingoing[3])
+		@test !isassigned(g.g1.ingoing, 1) || isempty(g.g1.ingoing[1])
+		@test (3 in g.g1.ingoing[2]) && 1 == length(g.g1.ingoing[2])
+		@test (2 in g.g1.ingoing[3]) && 1 == length(g.g1.ingoing[3])
+		@test (2 in g.g2.outgoing[1]) && 1 == length(g.g2.outgoing[1])
+		@test !isassigned(g.g2.outgoing, 2) || isempty(g.g2.outgoing[2])
+		@test !isassigned(g.g2.outgoing, 3) || isempty(g.g2.outgoing[3])
+		@test !isassigned(g.g1.outgoing, 1) || isempty(g.g1.outgoing[1])
+		@test (3 in g.g1.outgoing[2]) && 1 == length(g.g1.outgoing[2])
+		@test (2 in g.g1.outgoing[3]) && 1 == length(g.g1.outgoing[3])
+		@test 0 == g.g1.deltaplus[1]
+		@test 1 == g.g1.deltaplus[2]
+		@test 1 == g.g1.deltaplus[3]
+		@test 1 == g.g2.deltaplus[1]
+		@test 0 == g.g2.deltaplus[2]
+		@test 0 == g.g2.deltaplus[3]
+		@test 0 == g.g1.deltaminus[1]
+		@test 1 == g.g1.deltaminus[2]
+		@test 1 == g.g1.deltaminus[3]
+		@test 0 == g.g2.deltaminus[1]
+		@test 1 == g.g2.deltaminus[2]
+		@test 0 == g.g2.deltaminus[3]
 	end
-	@test (1 in g.g2.adjlist[2]) && 1 == length(g.g2.adjlist[2])
-	@test (2 in g.g2.adjlist[1]) && 1 == length(g.g2.adjlist[1])
-	@test !isassigned(g.g2.adjlist, 3) || isempty(g.g2.adjlist[3])
-	@test !isassigned(g.g1.adjlist, 1) || isempty(g.g1.adjlist[1])
-	@test (3 in g.g1.adjlist[2]) && 1 == length(g.g1.adjlist[2])
-	@test (2 in g.g1.adjlist[3]) && 1 == length(g.g1.adjlist[3])
-	@test (1 in g.g2.ingoing[2]) && 1 == length(g.g2.ingoing[2])
-	@test !isassigned(g.g2.ingoing, 1) || isempty(g.g2.ingoing[1])
-	@test !isassigned(g.g2.ingoing, 3) || isempty(g.g2.ingoing[3])
-	@test !isassigned(g.g1.ingoing, 1) || isempty(g.g1.ingoing[1])
-	@test (3 in g.g1.ingoing[2]) && 1 == length(g.g1.ingoing[2])
-	@test (2 in g.g1.ingoing[3]) && 1 == length(g.g1.ingoing[3])
-	@test (2 in g.g2.outgoing[1]) && 1 == length(g.g2.outgoing[1])
-	@test !isassigned(g.g2.outgoing, 2) || isempty(g.g2.outgoing[2])
-	@test !isassigned(g.g2.outgoing, 3) || isempty(g.g2.outgoing[3])
-	@test !isassigned(g.g1.outgoing, 1) || isempty(g.g1.outgoing[1])
-	@test (3 in g.g1.outgoing[2]) && 1 == length(g.g1.outgoing[2])
-	@test (2 in g.g1.outgoing[3]) && 1 == length(g.g1.outgoing[3])
-	@test 0 == g.g1.deltaplus[1]
-	@test 1 == g.g1.deltaplus[2]
-	@test 1 == g.g1.deltaplus[3]
-	@test 1 == g.g2.deltaplus[1]
-	@test 0 == g.g2.deltaplus[2]
-	@test 0 == g.g2.deltaplus[3]
-	@test 0 == g.g1.deltaminus[1]
-	@test 1 == g.g1.deltaminus[2]
-	@test 1 == g.g1.deltaminus[3]
-	@test 0 == g.g2.deltaminus[1]
-	@test 1 == g.g2.deltaminus[2]
-	@test 0 == g.g2.deltaminus[3]
+
+	@testset "Alpha and Beta initialized correctly" begin
+		ingraph = SimpleDiGraph(4) # Graph from Meek Rule 3
+		add_edge!(ingraph, 1, 2)
+		add_edge!(ingraph, 2, 1)
+		add_edge!(ingraph, 1, 3)
+		add_edge!(ingraph, 3, 1)
+		add_edge!(ingraph, 1, 4)
+		add_edge!(ingraph, 4, 1)
+		add_edge!(ingraph, 2, 3)
+		add_edge!(ingraph, 4, 3)
+		g = standardsetup(ingraph)
+		@test g.alpha == [2, 0, 0, 0]
+		@test g.beta == [0, 0, 2, 0]
+	end
 end
 
 @testset "optimizedsetup" begin
-	ingraph = SimpleDiGraph(3)
-	add_edge!(ingraph, 1, 2)
-	add_edge!(ingraph, 2, 3)
-	add_edge!(ingraph, 3, 2)
-	g = optimizedsetup(ingraph)
-	for i = 1:3
-		@test 0 == g.alpha[i]
-		@test 0 == g.beta[i]
+	@testset "All values are set correctly" begin
+		ingraph = SimpleDiGraph(3)
+		add_edge!(ingraph, 1, 2)
+		add_edge!(ingraph, 2, 3)
+		add_edge!(ingraph, 3, 2)
+		g = optimizedsetup(ingraph)
+		for i = 1:3
+			@test 0 == g.alpha[i]
+			@test 0 == g.beta[i]
+		end
+		@test (1 in g.g2.adjlist[2]) && 1 == length(g.g2.adjlist[2])
+		@test (2 in g.g2.adjlist[1]) && 1 == length(g.g2.adjlist[1])
+		@test !isassigned(g.g2.adjlist, 3) || isempty(g.g2.adjlist[3])
+		@test !isassigned(g.g1.adjlist, 1) || isempty(g.g1.adjlist[1])
+		@test (3 in g.g1.adjlist[2]) && 1 == length(g.g1.adjlist[2])
+		@test (2 in g.g1.adjlist[3]) && 1 == length(g.g1.adjlist[3])
+		@test (1 in g.g2.ingoing[2]) && 1 == length(g.g2.ingoing[2])
+		@test !isassigned(g.g2.ingoing, 1) || isempty(g.g2.ingoing[1])
+		@test !isassigned(g.g2.ingoing, 3) || isempty(g.g2.ingoing[3])
+		@test !isassigned(g.g1.ingoing, 1) || isempty(g.g1.ingoing[1])
+		@test (3 in g.g1.ingoing[2]) && 1 == length(g.g1.ingoing[2])
+		@test (2 in g.g1.ingoing[3]) && 1 == length(g.g1.ingoing[3])
+		@test (2 in g.g2.outgoing[1]) && 1 == length(g.g2.outgoing[1])
+		@test !isassigned(g.g2.outgoing, 2) || isempty(g.g2.outgoing[2])
+		@test !isassigned(g.g2.outgoing, 3) || isempty(g.g2.outgoing[3])
+		@test !isassigned(g.g1.outgoing, 1) || isempty(g.g1.outgoing[1])
+		@test (3 in g.g1.outgoing[2]) && 1 == length(g.g1.outgoing[2])
+		@test (2 in g.g1.outgoing[3]) && 1 == length(g.g1.outgoing[3])
+		@test 0 == g.g1.deltaplus[1]
+		@test 1 == g.g1.deltaplus[2]
+		@test 1 == g.g1.deltaplus[3]
+		@test 1 == g.g2.deltaplus[1]
+		@test 0 == g.g2.deltaplus[2]
+		@test 0 == g.g2.deltaplus[3]
+		@test 0 == g.g1.deltaminus[1]
+		@test 1 == g.g1.deltaminus[2]
+		@test 1 == g.g1.deltaminus[3]
+		@test 0 == g.g2.deltaminus[1]
+		@test 1 == g.g2.deltaminus[2]
+		@test 0 == g.g2.deltaminus[3]
 	end
-	@test (1 in g.g2.adjlist[2]) && 1 == length(g.g2.adjlist[2])
-	@test (2 in g.g2.adjlist[1]) && 1 == length(g.g2.adjlist[1])
-	@test !isassigned(g.g2.adjlist, 3) || isempty(g.g2.adjlist[3])
-	@test !isassigned(g.g1.adjlist, 1) || isempty(g.g1.adjlist[1])
-	@test (3 in g.g1.adjlist[2]) && 1 == length(g.g1.adjlist[2])
-	@test (2 in g.g1.adjlist[3]) && 1 == length(g.g1.adjlist[3])
-	@test (1 in g.g2.ingoing[2]) && 1 == length(g.g2.ingoing[2])
-	@test !isassigned(g.g2.ingoing, 1) || isempty(g.g2.ingoing[1])
-	@test !isassigned(g.g2.ingoing, 3) || isempty(g.g2.ingoing[3])
-	@test !isassigned(g.g1.ingoing, 1) || isempty(g.g1.ingoing[1])
-	@test (3 in g.g1.ingoing[2]) && 1 == length(g.g1.ingoing[2])
-	@test (2 in g.g1.ingoing[3]) && 1 == length(g.g1.ingoing[3])
-	@test (2 in g.g2.outgoing[1]) && 1 == length(g.g2.outgoing[1])
-	@test !isassigned(g.g2.outgoing, 2) || isempty(g.g2.outgoing[2])
-	@test !isassigned(g.g2.outgoing, 3) || isempty(g.g2.outgoing[3])
-	@test !isassigned(g.g1.outgoing, 1) || isempty(g.g1.outgoing[1])
-	@test (3 in g.g1.outgoing[2]) && 1 == length(g.g1.outgoing[2])
-	@test (2 in g.g1.outgoing[3]) && 1 == length(g.g1.outgoing[3])
-	@test 0 == g.g1.deltaplus[1]
-	@test 1 == g.g1.deltaplus[2]
-	@test 1 == g.g1.deltaplus[3]
-	@test 1 == g.g2.deltaplus[1]
-	@test 0 == g.g2.deltaplus[2]
-	@test 0 == g.g2.deltaplus[3]
-	@test 0 == g.g1.deltaminus[1]
-	@test 1 == g.g1.deltaminus[2]
-	@test 1 == g.g1.deltaminus[3]
-	@test 0 == g.g2.deltaminus[1]
-	@test 1 == g.g2.deltaminus[2]
-	@test 0 == g.g2.deltaminus[3]
+
+	@testset "Alpha and Beta initialized correctly" begin
+		ingraph = SimpleDiGraph(4) # Graph from Meek Rule 3
+		add_edge!(ingraph, 1, 2)
+		add_edge!(ingraph, 2, 1)
+		add_edge!(ingraph, 1, 3)
+		add_edge!(ingraph, 3, 1)
+		add_edge!(ingraph, 1, 4)
+		add_edge!(ingraph, 4, 1)
+		add_edge!(ingraph, 2, 3)
+		add_edge!(ingraph, 4, 3)
+		g = optimizedsetup(ingraph)
+		@test g.alpha == [2, 0, 0, 0]
+		@test g.beta == [0, 0, 2, 0]
+	end
 end
