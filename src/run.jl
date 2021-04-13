86b1a8fadccfa764f0bd9e76a9f5dcc159a76ecf
Malte311 <malte.luttermann@student.uni-luebeck.de>
2021-04-13 08:20:30 +0200

    Set parameters

--- src/run.jl
+++ src/run.jl
@@ -1,7 +1,7 @@
 using BenchmarkTools, Dates, JSON, Logging, PdagExtendability
 
-BenchmarkTools.DEFAULT_PARAMETERS.samples = 100000
-BenchmarkTools.DEFAULT_PARAMETERS.evals = 1
+samples = 1000
+evals = 1
 
 if length(ARGS) != 1 || !isfile(ARGS[1])
 	@error "Run the script via 'julia run.jl <path/to/config.json>'."
@@ -24,17 +24,19 @@ for f in readdir(config["benchmarkdir"])
 		config["only_undirected"]
 	)
 
-	bench1 = @benchmark pdag2dag($pdag)
-	bench2 = @benchmark fastpdag2dag($pdag, false)
-	bench3 = @benchmark fastpdag2dag($pdag, true)
+	bench1 = @benchmark pdag2dag($pdag) samples=samples evals=evals
+	bench2 = @benchmark fastpdag2dag($pdag, false) samples=samples evals=evals
+	bench3 = @benchmark fastpdag2dag($pdag, true) samples=samples evals=evals
 
 	for b in [bench1, bench2, bench3]
 		@info "Minimum time: $(round(minimum(b.times), digits=2))"
 		@info "Median time:  $(round(median(b.times), digits=2))"
 		@info "Mean time:    $(round(mean(b.times), digits=2))"
 		@info "Maximum time: $(round(maximum(b.times), digits=2))"
-		@info "---------------------"
+		@info "--------------------------------------------------"
 	end
+
+	config["logtofile"] && flush(io)
 
 	config["visualize"] || continue
 
