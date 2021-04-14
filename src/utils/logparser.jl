df2f2901b02962467db5fca3993ce07185ab178c
Malte311 <malte.luttermann@student.uni-luebeck.de>
2021-04-14 10:40:03 +0200

    Automatic R code generation

--- src/utils/logparser.jl
+++ src/utils/logparser.jl
@@ -14,6 +14,9 @@ dictionary has the following structure:
 		"input2": ...
 	}
 }
+
+# Examples
+TODO
 """
 function get_times_dict(file::String)::Dict
 	io = open(file, "r")
@@ -50,24 +53,60 @@ If set to true (or ommitted), the median will be used, otherwise the mean.
 
 It is possible to set the file parameter in order to save the `csv`-formatted
 string in that given file.
+
+# Examples
+TODO
 """
-function dict_to_csv(dict::Dict, use_median::Bool = true, file::String = "")::String
+function dict_to_csv(dict::Dict; use_median::Bool = true, file::String = "")::String
 	csv_str = "Algorithm;Instance;Time\n"
 	val = use_median ? "median" : "mean"
+	algo = collect(keys(dict))[1]
 
-	for (key, value) in dict
-		csv_str = string(csv_str, algo, ";", key, ";", value[val][1], "\n")
-		csv_str = string(csv_str, algo, ";", key, ";", value[val][2], "\n")
-		csv_str = string(csv_str, algo, ";", key, ";", value[val][3], "\n")
+	for (key, value) in dict[collect(keys(dict))[1]]
+		csv_str = string(csv_str, algo, ";", key, ";", value[val], "\n")
 	end
 
 	if file != ""
 		open(file, "w+") do io
-			write(io, contents)
+			write(io, csv_str)
 		end
 	end
 
 	csv_str
+end
+
+function gen_plot_code(out_file::String, csv_files::String...)
+	rcode = "library(ggplot2)\n\n"
+
+	firstline = true
+	for csv in csv_files
+		if firstline
+			rcode = string(
+				rcode,
+				"data <- read.csv(file = \"$csv\", sep=\";\", dec=\".\")",
+				"\n"
+			)
+			firstline = false
+		else
+			rcode = string(
+				rcode,
+				"data <- data + read.csv(file = \"$csv\", sep=\";\", dec=\".\")",
+				"\n"
+			)
+		end
+	end
+
+	rcode = string(
+		rcode,
+		"\n",
+		"ggplot(data, aes(x = Instance, y = Time, fill = Algorithm))",
+		" + ",
+		"geom_bar(stat=\"identity\", position = \"dodge\")"
+	)
+
+	open(out_file, "w+") do io
+		write(io, rcode)
+	end
 end
 
 """
@@ -75,7 +114,20 @@ end
 
 Print the contents of a dictionary as a formatted JSON string
 with readable indentation.
+
+# Examples
+TODO
 """
 function print_dict(dict::Dict, io::Core.IO = stdout)
 	println(io, json(dict, 4))
 end
+
+
+
+#print_dict(get_times_dict("/home/user/logs/test.txt"))
+
+dict_to_csv(get_times_dict("/home/user/logs/pdag2dag.txt"), file = "pdag2dag.csv")
+dict_to_csv(get_times_dict("/home/user/logs/fastpdag2dag-true.txt"), file = "fastpdag2dag-true.csv")
+dict_to_csv(get_times_dict("/home/user/logs/fastpdag2dag-false.txt"), file = "fastpdag2dag-false.csv")
+
+gen_plot_code("rcode.R", "pdag2dag.csv", "fastpdag2dag-true.csv", "fastpdag2dag-false.csv")
