4574e1fb3bdc03051dd1981a871983c2c8ec9c2c
Malte311 <malte.luttermann@student.uni-luebeck.de>
2021-04-17 17:10:55 +0200

    Fix tests

--- test/extendability/new_algo_lg/test_new_algo_lg.jl
+++ test/extendability/new_algo_lg/test_new_algo_lg.jl
@@ -1,224 +1,210 @@
 @testset "fastpdag2dag_lg" begin
 	@testset "No changes for DAG inputs" begin
-		for optimize in [true, false]
-			input = SimpleDiGraph(2)
-			add_edge!(input, 1, 2)
-			out = fastpdag2dag_lg(input, optimize)
-			@test nv(out) == 2 && ne(out) == 1 && has_edge(out, 1, 2)
+		input = SimpleDiGraph(2)
+		add_edge!(input, 1, 2)
+		out = fastpdag2dag_lg(input)
+		@test nv(out) == 2 && ne(out) == 1 && has_edge(out, 1, 2)
 
-			input = SimpleDiGraph(3)
-			add_edge!(input, 1, 2)
-			add_edge!(input, 2, 3)
-			out = fastpdag2dag_lg(input, optimize)
-			@test nv(out) == 3 && ne(out) == 2 && has_edge(out, 1, 2) &&
-				has_edge(out, 2, 3)
+		input = SimpleDiGraph(3)
+		add_edge!(input, 1, 2)
+		add_edge!(input, 2, 3)
+		out = fastpdag2dag_lg(input)
+		@test nv(out) == 3 && ne(out) == 2 && has_edge(out, 1, 2) &&
+			has_edge(out, 2, 3)
 
-			for n in [50, 100, 500]
-				input = SimpleDiGraph(n)
-				for i = 1:n-1
-					add_edge!(input, i, i+1)
-				end
-				output = fastpdag2dag_lg(input, optimize)
-				@test input == output
+		for n in [50, 100, 500]
+			input = SimpleDiGraph(n)
+			for i = 1:n-1
+				add_edge!(input, i, i+1)
 			end
+			output = fastpdag2dag_lg(input)
+			@test input == output
 		end
 	end
 
 	@testset "Meek Rule R1" begin
-		for optimize in [true, false]
-			input = SimpleDiGraph(3)
-			add_edge!(input, 1, 2)
-			add_edge!(input, 2, 3)
-			add_edge!(input, 3, 2)
-			out = fastpdag2dag_lg(input, optimize)
-			@test nv(out) == 3 && ne(out) == 2 && has_edge(out, 1, 2) &&
-				has_edge(out, 2, 3)
-		end
+		input = SimpleDiGraph(3)
+		add_edge!(input, 1, 2)
+		add_edge!(input, 2, 3)
+		add_edge!(input, 3, 2)
+		out = fastpdag2dag_lg(input)
+		@test nv(out) == 3 && ne(out) == 2 && has_edge(out, 1, 2) &&
+			has_edge(out, 2, 3)
 	end
 
 	@testset "Meek Rule R2" begin
-		for optimize in [true, false]
-			input = SimpleDiGraph(3)
-			add_edge!(input, 1, 2)
-			add_edge!(input, 1, 3)
-			add_edge!(input, 3, 1)
-			add_edge!(input, 2, 3)
-			out = fastpdag2dag_lg(input, optimize)
-			@test nv(out) == 3 && ne(out) == 3 && has_edge(out, 1, 2) &&
-				has_edge(out, 1, 3) && has_edge(out, 2, 3)
-		end
+		input = SimpleDiGraph(3)
+		add_edge!(input, 1, 2)
+		add_edge!(input, 1, 3)
+		add_edge!(input, 3, 1)
+		add_edge!(input, 2, 3)
+		out = fastpdag2dag_lg(input)
+		@test nv(out) == 3 && ne(out) == 3 && has_edge(out, 1, 2) &&
+			has_edge(out, 1, 3) && has_edge(out, 2, 3)
 	end
 
 	@testset "Meek Rule R3" begin
-		for optimize in [true, false]
-			g = SimpleDiGraph(4)
-			add_edge!(g, 1, 2)
-			add_edge!(g, 2, 1)
-			add_edge!(g, 1, 3)
-			add_edge!(g, 3, 1)
-			add_edge!(g, 1, 4)
-			add_edge!(g, 4, 1)
-			add_edge!(g, 2, 3)
-			add_edge!(g, 4, 3)
-			
-			out = fastpdag2dag_lg(g, optimize)
-			@test nv(out) == 4 && ne(out) == 5 && has_edge(out, 1, 3) &&
-				has_edge(out, 1, 4) && has_edge(out, 2, 1) && has_edge(out, 2, 3) &&
-				has_edge(out, 4, 3)
-		end
+		g = SimpleDiGraph(4)
+		add_edge!(g, 1, 2)
+		add_edge!(g, 2, 1)
+		add_edge!(g, 1, 3)
+		add_edge!(g, 3, 1)
+		add_edge!(g, 1, 4)
+		add_edge!(g, 4, 1)
+		add_edge!(g, 2, 3)
+		add_edge!(g, 4, 3)
+		
+		out = fastpdag2dag_lg(g)
+		@test nv(out) == 4 && ne(out) == 5 && has_edge(out, 1, 3) &&
+			has_edge(out, 1, 4) && has_edge(out, 2, 1) && has_edge(out, 2, 3) &&
+			has_edge(out, 4, 3)
 	end
 
 	@testset "Meek Rule R4" begin
-		for optimize in [true, false]
-			input = SimpleDiGraph(4)
-			add_edge!(input, 1, 2)
-			add_edge!(input, 2, 1)
-			add_edge!(input, 1, 3)
-			add_edge!(input, 3, 1)
-			add_edge!(input, 1, 4)
-			add_edge!(input, 4, 1)
-			add_edge!(input, 3, 2)
-			add_edge!(input, 4, 3)
-			out = fastpdag2dag_lg(input, optimize)
-			@test nv(out) == 4 && ne(out) == 5 && has_edge(out, 1, 2) &&
-				has_edge(out, 1, 3) && has_edge(out, 1, 4) && has_edge(out, 3, 2) &&
-				has_edge(out, 4, 3)
-		end
+		input = SimpleDiGraph(4)
+		add_edge!(input, 1, 2)
+		add_edge!(input, 2, 1)
+		add_edge!(input, 1, 3)
+		add_edge!(input, 3, 1)
+		add_edge!(input, 1, 4)
+		add_edge!(input, 4, 1)
+		add_edge!(input, 3, 2)
+		add_edge!(input, 4, 3)
+		out = fastpdag2dag_lg(input)
+		@test nv(out) == 4 && ne(out) == 5 && has_edge(out, 1, 2) &&
+			has_edge(out, 1, 3) && has_edge(out, 1, 4) && has_edge(out, 3, 2) &&
+			has_edge(out, 4, 3)
 	end
 
 	@testset "More PDAGs with possible extensions" begin
-		for optimize in [true, false]
-			input = SimpleDiGraph(4)
-			add_edge!(input, 1, 2)
-			add_edge!(input, 2, 3)
-			add_edge!(input, 3, 2)
-			add_edge!(input, 3, 4)
-			out = fastpdag2dag_lg(input, optimize)
-			@test nv(out) == 4 && ne(out) == 3 && has_edge(out, 1, 2) &&
-				has_edge(out, 2, 3) && has_edge(out, 3, 4)
+		input = SimpleDiGraph(4)
+		add_edge!(input, 1, 2)
+		add_edge!(input, 2, 3)
+		add_edge!(input, 3, 2)
+		add_edge!(input, 3, 4)
+		out = fastpdag2dag_lg(input)
+		@test nv(out) == 4 && ne(out) == 3 && has_edge(out, 1, 2) &&
+			has_edge(out, 2, 3) && has_edge(out, 3, 4)
 
-			for n in [20, 50, 100]
-				input = SimpleDiGraph(n)
-				for i = 1:n-1
-					add_edge!(input, i, i+1)
-					i % 2 == 0 && add_edge!(input, i+1, i)
-				end
-				out = fastpdag2dag_lg(input, optimize)
-				@test nv(out) == n && ne(out) == n-1
-				isok = true
-				for i = 1:n-1
-					isok &= has_edge(out, i, i+1) && !has_edge(out, i+1, i)
-				end
-				@test isok
+		for n in [20, 50, 100]
+			input = SimpleDiGraph(n)
+			for i = 1:n-1
+				add_edge!(input, i, i+1)
+				i % 2 == 0 && add_edge!(input, i+1, i)
 			end
-
-			input = SimpleDiGraph(5)
-			add_edge!(input, 1, 4)
-			add_edge!(input, 4, 1)
-			add_edge!(input, 4, 5)
-			add_edge!(input, 5, 4)
-			add_edge!(input, 5, 2)
-			add_edge!(input, 5, 3)
-			add_edge!(input, 2, 3)
-			add_edge!(input, 3, 2)
-			out = fastpdag2dag_lg(input, optimize)
-			@test nv(out) == 5 && ne(out) == 5 && has_edge(out, 1, 4) &&
-				has_edge(out, 4, 5) && has_edge(out, 5, 2) &&
-				has_edge(out, 5, 3) && has_edge(out, 2, 3)
-
-			input = SimpleDiGraph(5)
-			add_edge!(input, 1, 4)
-			add_edge!(input, 4, 1)
-			add_edge!(input, 5, 4)
-			add_edge!(input, 5, 2)
-			add_edge!(input, 5, 3)
-			add_edge!(input, 2, 3)
-			add_edge!(input, 3, 2)
-			out = fastpdag2dag_lg(input, optimize)
-			@test nv(out) == 5 && ne(out) == 5 && has_edge(out, 4, 1) &&
-				has_edge(out, 5, 4) && has_edge(out, 5, 2) &&
-				has_edge(out, 5, 3) && has_edge(out, 2, 3)
-
-			input = SimpleDiGraph(5)
-			add_edge!(input, 1, 4)
-			add_edge!(input, 4, 5)
-			add_edge!(input, 5, 4)
-			add_edge!(input, 5, 2)
-			add_edge!(input, 5, 3)
-			add_edge!(input, 2, 3)
-			add_edge!(input, 3, 2)
-			out = fastpdag2dag_lg(input, optimize)
-			@test nv(out) == 5 && ne(out) == 5 && has_edge(out, 1, 4) &&
-				has_edge(out, 4, 5) && has_edge(out, 5, 2) &&
-				has_edge(out, 5, 3) && has_edge(out, 2, 3)
-
-			input = SimpleDiGraph(5)
-			add_edge!(input, 1, 4)
-			add_edge!(input, 4, 1)
-			add_edge!(input, 4, 5)
-			add_edge!(input, 5, 4)
-			add_edge!(input, 5, 2)
-			add_edge!(input, 2, 5)
-			add_edge!(input, 5, 3)
-			add_edge!(input, 3, 5)
-			add_edge!(input, 2, 3)
-			add_edge!(input, 3, 2)
-			out = fastpdag2dag_lg(input, optimize)
-			@test nv(out) == 5 && ne(out) == 5 && has_edge(out, 1, 4) &&
-				has_edge(out, 2, 3) && has_edge(out, 4, 5) &&
-				has_edge(out, 5, 2) && has_edge(out, 5, 3)
+			out = fastpdag2dag_lg(input)
+			@test nv(out) == n && ne(out) == n-1
+			isok = true
+			for i = 1:n-1
+				isok &= has_edge(out, i, i+1) && !has_edge(out, i+1, i)
+			end
+			@test isok
 		end
+
+		input = SimpleDiGraph(5)
+		add_edge!(input, 1, 4)
+		add_edge!(input, 4, 1)
+		add_edge!(input, 4, 5)
+		add_edge!(input, 5, 4)
+		add_edge!(input, 5, 2)
+		add_edge!(input, 5, 3)
+		add_edge!(input, 2, 3)
+		add_edge!(input, 3, 2)
+		out = fastpdag2dag_lg(input)
+		@test nv(out) == 5 && ne(out) == 5 && has_edge(out, 1, 4) &&
+			has_edge(out, 4, 5) && has_edge(out, 5, 2) &&
+			has_edge(out, 5, 3) && has_edge(out, 2, 3)
+
+		input = SimpleDiGraph(5)
+		add_edge!(input, 1, 4)
+		add_edge!(input, 4, 1)
+		add_edge!(input, 5, 4)
+		add_edge!(input, 5, 2)
+		add_edge!(input, 5, 3)
+		add_edge!(input, 2, 3)
+		add_edge!(input, 3, 2)
+		out = fastpdag2dag_lg(input)
+		@test nv(out) == 5 && ne(out) == 5 && has_edge(out, 4, 1) &&
+			has_edge(out, 5, 4) && has_edge(out, 5, 2) &&
+			has_edge(out, 5, 3) && has_edge(out, 2, 3)
+
+		input = SimpleDiGraph(5)
+		add_edge!(input, 1, 4)
+		add_edge!(input, 4, 5)
+		add_edge!(input, 5, 4)
+		add_edge!(input, 5, 2)
+		add_edge!(input, 5, 3)
+		add_edge!(input, 2, 3)
+		add_edge!(input, 3, 2)
+		out = fastpdag2dag_lg(input)
+		@test nv(out) == 5 && ne(out) == 5 && has_edge(out, 1, 4) &&
+			has_edge(out, 4, 5) && has_edge(out, 5, 2) &&
+			has_edge(out, 5, 3) && has_edge(out, 2, 3)
+
+		input = SimpleDiGraph(5)
+		add_edge!(input, 1, 4)
+		add_edge!(input, 4, 1)
+		add_edge!(input, 4, 5)
+		add_edge!(input, 5, 4)
+		add_edge!(input, 5, 2)
+		add_edge!(input, 2, 5)
+		add_edge!(input, 5, 3)
+		add_edge!(input, 3, 5)
+		add_edge!(input, 2, 3)
+		add_edge!(input, 3, 2)
+		out = fastpdag2dag_lg(input)
+		@test nv(out) == 5 && ne(out) == 5 && has_edge(out, 1, 4) &&
+			has_edge(out, 2, 3) && has_edge(out, 4, 5) &&
+			has_edge(out, 5, 2) && has_edge(out, 5, 3)
 	end
 
 	@testset "Empty graph if no consistent extension is possible" begin
-		for optimize in [true, false]
-			input = SimpleDiGraph(4)
-			add_edge!(input, 1, 2)
-			add_edge!(input, 2, 1)
-			add_edge!(input, 1, 3)
-			add_edge!(input, 3, 1)
-			add_edge!(input, 2, 4)
-			add_edge!(input, 4, 2)
-			add_edge!(input, 3, 4)
-			add_edge!(input, 4, 3)
-			out = fastpdag2dag_lg(input, optimize)
-			@test nv(out) == 0 && ne(out) == 0
+		input = SimpleDiGraph(4)
+		add_edge!(input, 1, 2)
+		add_edge!(input, 2, 1)
+		add_edge!(input, 1, 3)
+		add_edge!(input, 3, 1)
+		add_edge!(input, 2, 4)
+		add_edge!(input, 4, 2)
+		add_edge!(input, 3, 4)
+		add_edge!(input, 4, 3)
+		out = fastpdag2dag_lg(input)
+		@test nv(out) == 0 && ne(out) == 0
 
-			input = SimpleDiGraph(4)
-			add_edge!(input, 1, 4)
-			add_edge!(input, 2, 1)
-			add_edge!(input, 2, 4)
-			add_edge!(input, 4, 2)
-			add_edge!(input, 3, 1)
-			add_edge!(input, 3, 4)
-			add_edge!(input, 4, 3)
-			out = fastpdag2dag_lg(input, optimize)
-			@test nv(out) == 0 && ne(out) == 0
+		input = SimpleDiGraph(4)
+		add_edge!(input, 1, 4)
+		add_edge!(input, 2, 1)
+		add_edge!(input, 2, 4)
+		add_edge!(input, 4, 2)
+		add_edge!(input, 3, 1)
+		add_edge!(input, 3, 4)
+		add_edge!(input, 4, 3)
+		out = fastpdag2dag_lg(input)
+		@test nv(out) == 0 && ne(out) == 0
 
-			for n in [50, 100, 500]
-				input = SimpleDiGraph(n)
-				for i = 1:n-1
-					add_edge!(input, i, i+1)
-					add_edge!(input, i+1, i)
-				end
-				add_edge!(input, 1, n)
-				add_edge!(input, n, 1)
-				output = fastpdag2dag_lg(input, optimize)
-				@test nv(output) == 0 && ne(output) == 0
+		for n in [50, 100, 500]
+			input = SimpleDiGraph(n)
+			for i = 1:n-1
+				add_edge!(input, i, i+1)
+				add_edge!(input, i+1, i)
 			end
-
-			input = SimpleDiGraph(5)
-			add_edge!(input, 1, 2)
-			add_edge!(input, 2, 1)
-			add_edge!(input, 1, 4)
-			add_edge!(input, 4, 5)
-			add_edge!(input, 5, 4)
-			add_edge!(input, 5, 2)
-			add_edge!(input, 5, 3)
-			add_edge!(input, 2, 3)
-			add_edge!(input, 3, 2)
-			out = fastpdag2dag_lg(input, optimize)
-			@test nv(out) == 0 && ne(out) == 0
+			add_edge!(input, 1, n)
+			add_edge!(input, n, 1)
+			output = fastpdag2dag_lg(input)
+			@test nv(output) == 0 && ne(output) == 0
 		end
+
+		input = SimpleDiGraph(5)
+		add_edge!(input, 1, 2)
+		add_edge!(input, 2, 1)
+		add_edge!(input, 1, 4)
+		add_edge!(input, 4, 5)
+		add_edge!(input, 5, 4)
+		add_edge!(input, 5, 2)
+		add_edge!(input, 5, 3)
+		add_edge!(input, 2, 3)
+		add_edge!(input, 3, 2)
+		out = fastpdag2dag_lg(input)
+		@test nv(out) == 0 && ne(out) == 0
 	end
 end
