b26e54d35a317c0835492b4d29c63aeaf4f2519c
Malte311 <malte.luttermann@student.uni-luebeck.de>
2021-04-17 15:48:05 +0200

    Add inbounds macro for performance

--- src/extendability/new_algo_lg/new_algo_datastructure_lg.jl
+++ src/extendability/new_algo_lg/new_algo_datastructure_lg.jl
@@ -48,8 +48,8 @@ end
 function insert_arc_lg!(graph::Graph, u::Int64, v::Int64)
 	add_edge!(graph.g, u, v)
 
-	graph.deltaplus_dir[u] += 1
-	graph.deltaminus_dir[v] += 1
+	@inbounds graph.deltaplus_dir[u] += 1
+	@inbounds graph.deltaminus_dir[v] += 1
 
 	update_alphabeta_lg!(graph, u, v, +1, true)
 end
@@ -59,10 +59,10 @@ function insert_edge_lg!(graph::Graph, u::Int64, v::Int64)
 	add_edge!(graph.g, u, v)
 	add_edge!(graph.g, v, u)
 
-	graph.deltaplus_undir[u] += 1
-	graph.deltaminus_undir[v] += 1
-	graph.deltaplus_undir[v] += 1
-	graph.deltaminus_undir[u] += 1
+	@inbounds graph.deltaplus_undir[u] += 1
+	@inbounds graph.deltaminus_undir[v] += 1
+	@inbounds graph.deltaplus_undir[v] += 1
+	@inbounds graph.deltaminus_undir[u] += 1
 
 	update_alphabeta_lg!(graph, u, v, +1, false)
 end
@@ -71,8 +71,8 @@ end
 function remove_arc_lg!(graph::Graph, u::Int64, v::Int64)
 	rem_edge!(graph.g, u, v)
 
-	graph.deltaplus_dir[u] -= 1
-	graph.deltaminus_dir[v] -= 1
+	@inbounds graph.deltaplus_dir[u] -= 1
+	@inbounds graph.deltaminus_dir[v] -= 1
 
 	update_alphabeta_lg!(graph, u, v, -1, true)
 end
@@ -82,10 +82,10 @@ function remove_edge_lg!(graph::Graph, u::Int64, v::Int64)
 	rem_edge!(graph.g, u, v)
 	rem_edge!(graph.g, v, u)
 
-	graph.deltaplus_undir[u] -= 1
-	graph.deltaminus_undir[v] -= 1
-	graph.deltaplus_undir[v] -= 1
-	graph.deltaminus_undir[u] -= 1
+	@inbounds graph.deltaplus_undir[u] -= 1
+	@inbounds graph.deltaminus_undir[v] -= 1
+	@inbounds graph.deltaplus_undir[v] -= 1
+	@inbounds graph.deltaminus_undir[u] -= 1
 
 	update_alphabeta_lg!(graph, u, v, -1, false)
 end
@@ -98,24 +98,24 @@ function update_alphabeta_lg!(g::Graph, u::Int64, v::Int64, val::Int64, is_uv_di
 		is_ux_undir = is_undirected_lg(g, u, x)
 		is_vx_undir = is_undirected_lg(g, v, x)
 
-		!is_uv_dir && is_ux_undir && (g.alpha[u] += val)
-		!is_uv_dir && is_directed_lg(g, x, u) && (g.beta[u] += val)
+		!is_uv_dir && is_ux_undir && @inbounds (g.alpha[u] += val)
+		!is_uv_dir && is_directed_lg(g, x, u) && @inbounds (g.beta[u] += val)
 
-		!is_uv_dir && is_vx_undir && (g.alpha[v] += val)
-		is_uv_dir  && is_vx_undir && (g.beta[v] += val)
-		!is_uv_dir && is_directed_lg(g, x, v) && (g.beta[v] += val)
+		!is_uv_dir && is_vx_undir && @inbounds (g.alpha[v] += val)
+		is_uv_dir  && is_vx_undir && @inbounds (g.beta[v] += val)
+		!is_uv_dir && is_directed_lg(g, x, v) && @inbounds (g.beta[v] += val)
 
-		is_ux_undir && is_vx_undir && (g.alpha[x] += val)
-		is_vx_undir && is_directed_lg(g, u, x) && (g.beta[x] += val)
-		is_ux_undir && is_directed_lg(g, v, x) && (g.beta[x] += val)
+		is_ux_undir && is_vx_undir && @inbounds (g.alpha[x] += val)
+		is_vx_undir && is_directed_lg(g, u, x) && @inbounds (g.beta[x] += val)
+		is_ux_undir && is_directed_lg(g, v, x) && @inbounds (g.beta[x] += val)
 	end
 end
 
 
 function is_ps_lg(g::Graph, s::Int64)::Bool
-	g.deltaplus_dir[s] == 0 &&
-	g.beta[s] == g.deltaplus_undir[s] * g.deltaminus_dir[s] &&
-	g.alpha[s] == binomial(g.deltaplus_undir[s], 2)
+	@inbounds g.deltaplus_dir[s] == 0 &&
+	@inbounds g.beta[s] == g.deltaplus_undir[s] * g.deltaminus_dir[s] &&
+	@inbounds g.alpha[s] == binomial(g.deltaplus_undir[s], 2)
 end
 
 
@@ -140,13 +140,13 @@ function pop_ps_lg!(graph::Graph, s::Int64)::Vector{Int64}
 
 		# Since s is a sink, all outneighbors have undirected edges.
 		for undir in outneighbors(graph.g, s)
-			is_undirected_lg(graph, ingoing, undir) && (graph.alpha[undir] += -1)
-			is_directed_lg(graph, ingoing, undir) && (graph.beta[undir] += -1)
+			is_undirected_lg(graph, ingoing, undir) && @inbounds (graph.alpha[undir] += -1)
+			is_directed_lg(graph, ingoing, undir) && @inbounds (graph.beta[undir] += -1)
 		end
 
 		rem_edge!(graph.g, ingoing, s)
-		graph.deltaplus_dir[ingoing] -= 1
-		graph.deltaminus_dir[s] -= 1
+		@inbounds graph.deltaplus_dir[ingoing] -= 1
+		@inbounds graph.deltaminus_dir[s] -= 1
 	end
 
 	# Delete undirected edges incident to s.
