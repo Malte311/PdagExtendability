f8fc699a60f2d7dcc66ba9f2ce3761eefd7675b7
Malte311 <malte.luttermann@student.uni-luebeck.de>
2021-04-24 09:48:16 +0200

    Add utils for tests

--- src/utils/utils.jl
+++ src/utils/utils.jl
@@ -1,3 +1,27 @@
+using LightGraphs
+
+
+function isacyclic(g::SimpleDiGraph)::Bool
+	!is_cyclic(g)
+end
+
+
+function vstructures(g::SimpleDiGraph)::Vector{Tuple{Int64, Int64, Int64}}
+	result = Vector{Tuple{Int64, Int64, Int64}}()
+	for u in vertices(g)
+		for v in vertices(g)
+			(u != v && has_edge(g, u, v)) || continue
+			for w in vertices(g)
+				(u != w && v != w && has_edge(g, w, v)) || continue
+				if !has_edge(g, u, w) && !has_edge(g, w, u)
+					push!(result, (u <= w ? u : w, v, u <= w ? w : u))
+				end
+			end
+		end
+	end
+	result
+end
+
 """
 	nanosec2sec(time::Float64)::Float64
 
