847447ea2c36eb61b5fcee7c826c657d5a4434ac
Malte311 <malte.luttermann@student.uni-luebeck.de>
2021-04-25 16:51:22 +0200

    Update tests

--- test/extendability/implementation_lg/test_new_algo_lg.jl
+++ test/extendability/implementation_lg/test_new_algo_lg.jl
@@ -3,7 +3,7 @@
 		for optimize in [true, false]
 			input = SimpleDiGraph(2)
 			add_edge!(input, 1, 2)
-			out = fastpdag2dag_lg(input)
+			out = fastpdag2dag_lg(input, optimize)
 			@test out == input
 		end
 	end
@@ -13,7 +13,7 @@
 			input = SimpleDiGraph(3)
 			add_edge!(input, 1, 2)
 			add_edge!(input, 2, 3)
-			out = fastpdag2dag_lg(input)
+			out = fastpdag2dag_lg(input, optimize)
 			@test out == input
 		end
 	end
@@ -25,7 +25,7 @@
 				for i = 1:n-1
 					add_edge!(input, i, i+1)
 				end
-				output = fastpdag2dag_lg(input)
+				output = fastpdag2dag_lg(input, optimize)
 				@test input == output
 			end
 		end
@@ -37,7 +37,7 @@
 			add_edge!(input, 1, 2)
 			add_edge!(input, 2, 3)
 			add_edge!(input, 3, 2)
-			out = fastpdag2dag_lg(input)
+			out = fastpdag2dag_lg(input, optimize)
 			@test nv(out) == 3 && ne(out) == 2 && has_edge(out, 1, 2) &&
 				has_edge(out, 2, 3)
 		end
@@ -50,7 +50,7 @@
 			add_edge!(input, 1, 3)
 			add_edge!(input, 3, 1)
 			add_edge!(input, 2, 3)
-			out = fastpdag2dag_lg(input)
+			out = fastpdag2dag_lg(input, optimize)
 			@test nv(out) == 3 && ne(out) == 3 && has_edge(out, 1, 2) &&
 				has_edge(out, 1, 3) && has_edge(out, 2, 3)
 		end
@@ -68,7 +68,7 @@
 			add_edge!(g, 2, 3)
 			add_edge!(g, 4, 3)
 			
-			out = fastpdag2dag_lg(g)
+			out = fastpdag2dag_lg(g, optimize)
 			@test is_consistent_extension(out, g)
 		end
 	end
@@ -84,7 +84,7 @@
 			add_edge!(input, 4, 1)
 			add_edge!(input, 3, 2)
 			add_edge!(input, 4, 3)
-			out = fastpdag2dag_lg(input)
+			out = fastpdag2dag_lg(input, optimize)
 			@test is_consistent_extension(out, input)
 		end
 	end
@@ -96,7 +96,7 @@
 			add_edge!(input, 2, 3)
 			add_edge!(input, 3, 2)
 			add_edge!(input, 3, 4)
-			out = fastpdag2dag_lg(input)
+			out = fastpdag2dag_lg(input, optimize)
 			@test is_consistent_extension(out, input)
 		end
 	end
@@ -109,7 +109,7 @@
 					add_edge!(input, i, i+1)
 					i % 2 == 0 && add_edge!(input, i+1, i)
 				end
-				out = fastpdag2dag_lg(input)
+				out = fastpdag2dag_lg(input, optimize)
 				@test is_consistent_extension(out, input)
 			end
 		end
@@ -126,7 +126,7 @@
 			add_edge!(input, 5, 3)
 			add_edge!(input, 2, 3)
 			add_edge!(input, 3, 2)
-			out = fastpdag2dag_lg(input)
+			out = fastpdag2dag_lg(input, optimize)
 			@test is_consistent_extension(out, input)
 		end
 	end
@@ -141,7 +141,7 @@
 			add_edge!(input, 5, 3)
 			add_edge!(input, 2, 3)
 			add_edge!(input, 3, 2)
-			out = fastpdag2dag_lg(input)
+			out = fastpdag2dag_lg(input, optimize)
 			@test is_consistent_extension(out, input)
 		end
 	end
@@ -156,7 +156,7 @@
 			add_edge!(input, 5, 3)
 			add_edge!(input, 2, 3)
 			add_edge!(input, 3, 2)
-			out = fastpdag2dag_lg(input)
+			out = fastpdag2dag_lg(input, optimize)
 			@test is_consistent_extension(out, input)
 		end
 	end
@@ -174,7 +174,7 @@
 			add_edge!(input, 3, 5)
 			add_edge!(input, 2, 3)
 			add_edge!(input, 3, 2)
-			out = fastpdag2dag_lg(input)
+			out = fastpdag2dag_lg(input, optimize)
 			@test is_consistent_extension(out, input)
 		end
 	end
@@ -190,7 +190,7 @@
 			add_edge!(input, 4, 2)
 			add_edge!(input, 3, 4)
 			add_edge!(input, 4, 3)
-			out = fastpdag2dag_lg(input)
+			out = fastpdag2dag_lg(input, optimize)
 			@test out == SimpleDiGraph(0)
 		end
 	end
@@ -205,7 +205,7 @@
 			add_edge!(input, 3, 1)
 			add_edge!(input, 3, 4)
 			add_edge!(input, 4, 3)
-			out = fastpdag2dag_lg(input)
+			out = fastpdag2dag_lg(input, optimize)
 			@test out == SimpleDiGraph(0)
 		end
 	end
@@ -220,7 +220,7 @@
 				end
 				add_edge!(input, 1, n)
 				add_edge!(input, n, 1)
-				output = fastpdag2dag_lg(input)
+				output = fastpdag2dag_lg(input, optimize)
 				@test output == SimpleDiGraph(0)
 			end
 		end
@@ -238,7 +238,7 @@
 			add_edge!(input, 5, 3)
 			add_edge!(input, 2, 3)
 			add_edge!(input, 3, 2)
-			out = fastpdag2dag_lg(input)
+			out = fastpdag2dag_lg(input, optimize)
 			@test out == SimpleDiGraph(0)
 		end
 	end
