992ed1cf08ef775a5d68969a2eb733a60c511b7c
Malte311 <malte.luttermann@student.uni-luebeck.de>
2021-04-26 19:51:21 +0200

    Add more graph generators

--- src/utils/graph_generator.jl
+++ src/utils/graph_generator.jl
@@ -1,5 +1,25 @@
 using LightGraphs
 
+function friendshipgraph(n::Int64; filepath::String = "")::SimpleDiGraph
+	n % 2 == 0 || error("Invalid value: n has to be equal.")
+
+	g = SimpleDiGraph(n+1)
+
+	for i = 2:n+1
+		add_edge!(g, 1, i)
+		add_edge!(g, i, 1)
+	end
+
+	for i = 2:2:n
+		add_edge!(g, i, i+1)
+		add_edge!(g, i+1, i)
+	end
+
+	filepath != "" && save2file(g, filepath)
+
+	g
+end
+
 """
 
 TODO
@@ -14,13 +34,43 @@ function stargraph(n::Int64; filepath::String = "")::SimpleDiGraph
 		add_edge!(g, i, 1)
 	end
 
-	if filepath != ""
-		open(filepath, "w+") do io
-			write(io, graph2str(g, is_only_undir = true))
-		end
-	end
+	filepath != "" && save2file(g, filepath)
 
 	g
+end
+
+"""
+TODO
+
+n is inner n, total 2n
+
+sunletgraph(100000, filepath = "../benchmarks/dummy/sunlet-n=100000.txt")
+"""
+function sunletgraph(n::Int64; filepath::String = "")::SimpleDiGraph
+	g = SimpleDiGraph(2*n)
+
+	add_edge!(g, n, 1)
+	add_edge!(g, 1, n)
+
+	for i = 1:n-1
+		add_edge!(g, i, i+1)
+		add_edge!(g, i+1, i)
+	end
+
+	for i = 1:n
+		add_edge!(g, i, i+n)
+		add_edge!(g, i+n, i)
+	end
+
+	filepath != "" && save2file(g, filepath)
+
+	g
+end
+
+function save2file(g::SimpleDiGraph, file::String; is_only_undir::Bool = true)
+	open(file, "w+") do io
+		write(io, graph2str(g, is_only_undir = is_only_undir))
+	end
 end
 
 """
