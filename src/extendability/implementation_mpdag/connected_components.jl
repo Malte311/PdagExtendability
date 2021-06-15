208b9b04bb8488284d871dec5ee23ba026082d8b
Malte311 <malte.luttermann@student.uni-luebeck.de>
2021-06-15 15:05:58 +0200

    Fix bucket algorithm

--- src/extendability/implementation_mpdag/connected_components.jl
+++ src/extendability/implementation_mpdag/connected_components.jl
@@ -4,69 +4,35 @@
 """
 TODO
 """
-function buckets!(g::DtGraph)::Vector{Set{Int64}}
-	components = Vector{Int}(undef, g.numvertices)
-	oReps = Vector{Int64}()
-	oNodes = Vector{Int64}()
-	marked = falses(g.numvertices)
-	dfsNum = Vector{Int}(undef, g.numvertices)
-	dfsPos = 1
+function buckets(g::DtGraph)::Vector{Set{Int64}}
+	buckets = Vector{Set{Int64}}()
+	visited = falses(g.numvertices)
 
 	for u = 1:g.numvertices
-		if !marked[u]
-			marked[u] = true
-			traverse!(u, oReps, oNodes, dfsNum, dfsPos)
-			dfs!(u, oReps, oNodes, dfsNum, dfsPos, marked, components)
+		if !visited[u]
+			bucket = dfs!(g, u, visited)
+			length(bucket) > 1 && push!(buckets, bucket)
 		end
 	end
+
+	buckets
 end
 
 """
 TODO
 """
-function dfs!(v, oReps, oNodes, dfsNum, dfsPos, marked, components)
-	for w in undirected[v]
-		if marked[w]
-			handle_non_tree_edge!(w, oReps, oNodes, dfsNum)
-		else
-			traverse!(w, oReps, oNodes, dfsNum, dfsPos)
-			marked[w] = true
-			dfs!(w, oReps, oNodes, dfsNum, dfsPos, marked, components)
+function dfs!(g::DtGraph, u::Int64, visited::Vector{Bool})::Set{Int64}
+	bucket = Set{Int64}()
+	stack = Vector{Int64}([u])
+
+	while !isempty(stack)
+		v = pop!(stack)
+		visited[v] = true
+		push!(bucket, v)
+		for w in g.undirected[v]
+			!visited[w] && push!(stack, w)
 		end
 	end
-	backtrack!(v, oReps, oNodes, components)
-end
 
-"""
-TODO
-"""
-function traverse!(v, oReps, oNodes, dfsNum, dfsPos)
-	push!(v, oReps)
-	push!(v, oNodes)
-	dfsNum[v] = dfsPos
-	dfsPos += 1
-end
-
-"""
-TODO
-"""
-function handle_non_tree_edge!(v, oReps, oNodes, dfsNum)
-	if v in oNodes
-		while dfsNum[v] < dfsNum[last(oReps)]
-			pop!(oReps)
-		end
-	end
-end
-
-"""
-TODO
-"""
-function backtrack!(v, oReps, oNodes, components)
-	if v == last(oReps)
-		pop!(oReps)
-		while (w = pop!(oNodes)) != v
-			components[w] = v
-		end
-		components[v] = v
-	end
+	bucket
 end
