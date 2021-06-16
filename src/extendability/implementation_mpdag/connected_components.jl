97af193f33325b123ea445c2c0919e74b31d5453
Malte311 <malte.luttermann@student.uni-luebeck.de>
2021-06-16 16:53:24 +0200

    Add documentation

--- src/extendability/implementation_mpdag/connected_components.jl
+++ src/extendability/implementation_mpdag/connected_components.jl
@@ -2,7 +2,31 @@
 @isdefined(DtGraph) || include("../implementation_hs/dor_tarsi_algo_datastructure_hs.jl")
 
 """
-TODO
+	buckets(g::DtGraph)::Vector{Set{Int64}}
+
+Compute all buckets, i.e., maximum undirected subcomponents, of `g`.
+
+# References
+M. Wienöbst, M. Bannach, M. Liśkiewicz (2021).
+Extendability of Causal Graphical Models: Algorithms and Computational Complexity.
+37th Conference on Uncertainty in Artificial Intelligence, 2021 (UAI 2021).
+
+# Examples
+```julia-repl
+julia> g = SimpleDiGraph(3)
+{3, 0} directed simple Int64 graph
+julia> add_edge!(g, 1, 2)
+true
+julia> add_edge!(g, 2, 3)
+true
+julia> buckets(setup_hs(g))
+Set{Int64}[]
+julia> add_edge!(g, 3, 2)
+true
+julia> buckets(setup_hs(g))
+1-element Vector{Set{Int64}}:
+ Set([2, 3])
+```
 """
 function buckets(g::DtGraph)::Vector{Set{Int64}}
 	buckets = Vector{Set{Int64}}()
@@ -19,7 +43,32 @@ function buckets(g::DtGraph)::Vector{Set{Int64}}
 end
 
 """
-TODO
+	dfs!(g::DtGraph, u::Int64, visited::BitArray)::Set{Int64}
+
+Start a depth first search at vertex `u` in graph `g` and visit
+only previously unvisited vertices. `visited` specifies for each
+vertex whether it was visited before or not and is updated accordingly.
+The result is a set of vertices reachable from `u` via _undirected_
+edges only.
+
+# Examples
+```julia-repl
+julia> g = SimpleDiGraph(3)
+{3, 0} directed simple Int64 graph
+julia> add_edge!(g, 1, 2)
+true
+julia> add_edge!(g, 2, 3)
+true
+julia> add_edge!(g, 3, 2)
+true
+julia> dfs!(setup_hs(g), 1, falses(3))
+Set{Int64} with 1 element:
+  1
+julia> dfs!(setup_hs(g), 2, falses(3))
+Set{Int64} with 2 elements:
+  2
+  3
+```
 """
 function dfs!(g::DtGraph, u::Int64, visited::BitArray)::Set{Int64}
 	bucket = Set{Int64}()
--- src/extendability/implementation_mpdag/new_algo_mpdag.jl
+++ src/extendability/implementation_mpdag/new_algo_mpdag.jl
@@ -4,9 +4,28 @@ using LightGraphs
 @isdefined(buckets) || include("connected_components.jl")
 
 """
-TODO
+	mpdag2dag(g::SimpleDiGraph)::SimpleDiGraph
 
-Assumes that the input graph is an MPDAG and only works on MPDAGs.
+Convert a given MPDAG to a consistent DAG extension. Only works for MPDAGs.
+
+# References
+M. Wienöbst, M. Bannach, M. Liśkiewicz (2021).
+Extendability of Causal Graphical Models: Algorithms and Computational Complexity.
+37th Conference on Uncertainty in Artificial Intelligence, 2021 (UAI 2021).
+
+# Examples
+```julia-repl
+julia> g = SimpleDiGraph(3)
+{3, 0} directed simple Int64 graph
+julia> add_edge!(g, 1, 2)
+true
+julia> add_edge!(g, 2, 3)
+true
+julia> collect(edges(mpdag2dag(g)))
+2-element Vector{LightGraphs.SimpleGraphs.SimpleEdge{Int64}}:
+ Edge 1 => 2
+ Edge 2 => 3
+```
 """
 function mpdag2dag(g::SimpleDiGraph)::SimpleDiGraph
 	result = copy(g)
@@ -38,7 +57,35 @@ function mpdag2dag(g::SimpleDiGraph)::SimpleDiGraph
 end
 
 """
-TODO
+	subgraph(g::DtGraph, bucket::Set{Int64}, m::Dict)::DtGraph
+
+Compute the subgraph induces by the vertices in `bucket`.
+`m` is a mapping for vertices which is necessary if the
+vertices in `bucket` are not labelled from `1` to `length(bucket)`.
+It has to map each vertex in `bucket` to a number between `1` and
+`length(bucket)`.
+
+# Examples
+```julia-repl
+julia> g = SimpleDiGraph(3)
+{3, 0} directed simple Int64 graph
+julia> add_edge!(g, 1, 2)
+true
+julia> add_edge!(g, 2, 3)
+true
+julia> s = subgraph(setup_hs(g), Set([1, 2]), Dict(1 => 1, 2 => 2))
+DtGraph(
+	2,
+	Set([2, 1]),
+	Set{Int64}[],
+	Set{Int64}[Set(), Set([1])],
+	Set{Int64}[Set([2]), Set()],
+	Set{Int64}[Set(), Set()]
+)
+julia> collect(edges(dtgraph2digraph(s)))
+1-element Vector{LightGraphs.SimpleGraphs.SimpleEdge{Int64}}:
+ Edge 1 => 2
+```
 """
 function subgraph(g::DtGraph, bucket::Set{Int64}, m::Dict)::DtGraph
 	n = length(bucket)
@@ -69,7 +116,23 @@ function subgraph(g::DtGraph, bucket::Set{Int64}, m::Dict)::DtGraph
 end
 
 """
-TODO
+	amo(g::DtGraph)::Tuple{Vector{Int64}, Vector{Int64}}
+
+Compute an acyclic moral orientation (AMO) for a bucket in linear time.
+If the input graph does not admit an AMO, the result will not be an AMO
+but an arbitrary ordering. The result can be checked via [`isamo`](@ref).
+
+# Examples
+```julia-repl
+julia> g = SimpleDiGraph(3)
+{3, 0} directed simple Int64 graph
+julia> add_edge!(g, 1, 2)
+true
+julia> add_edge!(g, 2, 3)
+true
+julia> amo(setup_hs(g))
+([1, 2, 3], [1, 2, 3])
+```
 """
 function amo(g::DtGraph)::Tuple{Vector{Int64}, Vector{Int64}}
 	n = g.numvertices
@@ -114,7 +177,30 @@ function amo(g::DtGraph)::Tuple{Vector{Int64}, Vector{Int64}}
 end
 
 """
-TODO
+	isamo(g::DtGraph, ordering::Tuple{Vector{Int64}, Vector{Int64}})::Bool
+
+Check whether a given ordering is an acyclic moral orientation for a bucket.
+
+# Examples
+```julia-repl
+julia> g = SimpleDiGraph(3)
+{3, 0} directed simple Int64 graph
+julia> add_edge!(g, 1, 2)
+true
+julia> add_edge!(g, 2, 3)
+true
+julia> dtgraph = setup_hs(g)
+DtGraph(
+	3,
+	Set([2, 3, 1]),
+	Set{Int64}[],
+	Set{Int64}[Set(), Set([1]), Set([2])],
+	Set{Int64}[Set([2]), Set([3]), Set()],
+	Set{Int64}[Set(), Set(), Set()]
+)
+julia> isamo(dtgraph, amo(dtgraph))
+true
+```
 """
 function isamo(g::DtGraph, ordering::Tuple{Vector{Int64}, Vector{Int64}})::Bool
 	(orderindex, order) = ordering
